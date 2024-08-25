b5f233f889d73ed31a53f7d7aab2fc0ac48e788b
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-25 23:56:27 +0000

    alx-react-app-new

---
+++ alx-react-app-new/src/UserContext.js
@@ -0,0 +1,12 @@
+// src/UserContext.js
+import React, { createContext, useContext } from 'react';
+
+const UserContext = createContext();
+
+export function UserProvider({ children, value }) {
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+export function useUserContext() {
+  return useContext(UserContext);
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: 'lightgray' }}>
+      <p>Main content goes here...</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
