7b104aa6e9e6d36affa24f86618bd29988aa7a6e
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-01 02:26:26 +0000

    alx-react-app-recipe-sharing-app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
+import { useRecipeStore } from '../components/recipeStore';
+import RecipeDetails from './components/RecipeDetails';
 import RecipeDetails from './components/RecipeDetails';  // Example component
 
 function App() {
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecipeDetails = () => {
+  return <div>Recipe Details Component</div>;
+};
+
+export default RecipeDetails;
