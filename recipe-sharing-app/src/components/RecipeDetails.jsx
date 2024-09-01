08ecd41a9ea989503455e9436ab759ff8969d1f5
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-01 02:28:58 +0000

    alx-react-app-recipe-sharing-app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useRecipeStore } from '../recipeStore';
 
 const RecipeDetails = () => {
-  return <div>Recipe Details Component</div>;
+  const { id } = useParams();
+  const recipe = useRecipeStore(state => state.recipes.find(r => r.id === parseInt(id)));
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      {/* Add more recipe details here */}
+    </div>
+  );
 };
 
 export default RecipeDetails;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import useRecipeStore from '../recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../recipeStore';
+
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
-
-  return (
-    <div>
-      {filteredRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default RecipeList;
+    const recipes = useRecipeStore(state => state.filteredRecipes);
+  
+    return (
+      <div>
+        {recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>
+              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default RecipeList;
+  
