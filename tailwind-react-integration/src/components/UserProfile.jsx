c075a4872388078cd009af6147ca4f5af29f76da
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-15 21:40:36 +0000

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,7 +9,7 @@ function UserProfile() {
           alt="User"
           className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
+        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
           John Doe
         </h1>
         <p className="text-sm sm:text-base text-gray-600">
