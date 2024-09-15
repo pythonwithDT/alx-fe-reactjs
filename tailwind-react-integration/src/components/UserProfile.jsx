c4e2d2f95501ecfcaf55959072e73039a9ebd47f
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-15 21:35:13 +0000

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,24 +2,19 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <>
-      <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="User"
-          className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
-        />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
-          John Doe
-        </h1>
-        <p className="text-sm sm:text-base text-gray-600">
-          Developer at Example Co. Loves to write code and explore new technologies.
-        </p>
-      </div>
-      <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-        {/* Content for the second user-profile div */}
-      </div>
-    </>
+    <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+      />
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-base text-gray-600">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
+    </div>
   );
 }
 
