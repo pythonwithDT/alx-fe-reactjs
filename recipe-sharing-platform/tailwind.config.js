811abd725c98af210ce4d02e6f7bd2a1641e7e3d
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-09-22 23:59:41 +0000

    tailwind

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  purge: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'], // Add purge option
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
